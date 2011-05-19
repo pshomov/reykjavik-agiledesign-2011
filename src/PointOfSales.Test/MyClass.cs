27567db0b5859083bf122ce260498810c0801792
Petar Shomov <petar@spretturmarimo.is>
2011-05-19 11:56:55 +0000

    refactoring to move the catalog to the client

--- src/PointOfSales.Test/MyClass.cs
+++ src/PointOfSales.Test/MyClass.cs
@@ -10,18 +10,26 @@ namespace PointOfSales
 	{
 		[Test]
 		public void shouldDisplayPriceForProduct(){
+			Dictionary<string, string> priceCatalog = new Dictionary<string, string>() {
+				{"1212", "123,50 kr"}	
+			};
+
 			var display = Substitute.For<Display>();
-			var sale = new Sale(display);
+			var sale = new Sale(display, priceCatalog);
 			
 			sale.onBarcodeReceived("1212");
 			
-			display.Received().show("123,5 kr");
+			display.Received().show("123,50 kr");
 		}
 		
 		[Test]
 		public void shouldDisplayErrorWhenNoProduct() {
+			Dictionary<string, string> priceCatalog = new Dictionary<string, string>() {
+				{"iPhone", "Error: No such product"}	
+			};
+
 			var display = Substitute.For<Display>();
-			var sale = new Sale(display);
+			var sale = new Sale(display, priceCatalog);
 
 			sale.onBarcodeReceived("iPhone");
 			display.Received().show("Error: No such product");
@@ -29,11 +37,28 @@ namespace PointOfSales
 		
 		[Test]
 		public void shouldDisplayErrorWhenBarcodeIsEmpty() {
+			Dictionary<string, string> priceCatalog = new Dictionary<string, string>() {
+				{"", "Error: Barcode is empty, fix your scanner please."}	
+			};
+
 			var display = Substitute.For<Display>();
-			var sale = new Sale(display);
+			var sale = new Sale(display, priceCatalog);
 
 			sale.onBarcodeReceived("");
 			display.Received().show("Error: Barcode is empty, fix your scanner please.");
+		}
+		
+		[Test]
+		public void shouldDisplayPriceWithPrecisionOfTwo() {
+			Dictionary<string, string> priceCatalog = new Dictionary<string, string>() {
+				{"iPad", "122,00 kr"}	
+			};
+
+			var display = Substitute.For<Display>();
+			var sale = new Sale(display, priceCatalog);
+			
+			sale.onBarcodeReceived("iPad");
+			display.Received().show("122,00 kr");
 		}
 	}
 	
@@ -43,19 +68,15 @@ namespace PointOfSales
 	
 	public class Sale {
 		private Display display;
+		private Dictionary<string, string> catalog;
 		
-		public Sale(Display disp) {
+		public Sale(Display disp, Dictionary<string, string> cat) {
 			this.display = disp;	
+			this.catalog = cat;
 		}
 
-		public void onBarcodeReceived(string barcode){
-			Dictionary<string, string> priceCatalog = new Dictionary<string, string>(){
-				{"1212", "123,5 kr"},
-				{"iPhone", "Error: No such product"},
-				{"", "Error: Barcode is empty, fix your scanner please."}
-			};
-			
-			display.show(priceCatalog[barcode]);
+		public void onBarcodeReceived(string barcode){			
+			display.show(catalog[barcode]);
 		}
 	}
 }
