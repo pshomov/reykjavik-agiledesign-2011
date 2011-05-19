43d4aac9b954c8e82e499347eaf24f0f909749b5
Petar Shomov <petar@spretturmarimo.is>
2011-05-19 11:24:38 +0000

    displaying the price on the display is here, preparing for handling non-exisitng ones

--- src/PointOfSales.Test/MyClass.cs
+++ src/PointOfSales.Test/MyClass.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using NSubstitute;
 
 namespace PointOfSales
 {
@@ -7,7 +8,37 @@ namespace PointOfSales
 	public class MyClass
 	{
 		[Test]
-		public void should_do_that(){
+		public void shouldDisplayPriceForProduct(){
+			var display = Substitute.For<Display>();
+			var sale = new Sale(display);
+			
+			sale.onBarcodeReceived("1212");
+			
+			display.Received().show("123,5 kr");
+		}
+		
+		[Test]
+		public void shouldDisplayErrorWhenNoProduct() {
+			var display = Substitute.For<Display>();
+			var sale = new Sale(display);
+
+			sale.onBarcodeReceived("iPhone");
+			display.Received().show("Error: No such product");
+		}
+	}
+	
+	public interface Display{
+		void show(string message);
+	}
+	
+	public class Sale {
+		private Display display;
+		
+		public Sale(Display disp) {
+			this.display = disp;	
+		}
+		public void onBarcodeReceived(string barcode){
+			display.show("123,5 kr");
 		}
 	}
 }
